899627ea2d929b0ae48a371bf62ad68717e9cfd5
Mark Furland <mtfurlan@i3detroit.org>
2018-09-30 03:52:27 -0400

    shows BME temperature now

--- src/main.cpp
+++ src/main.cpp
@@ -6,66 +6,14 @@
 #include "Wire.h"
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 5, /* data=*/ 4);
-BME280 mySensor;
+BME280 bmeSensor;
 char buf[512];
 
 void setup()
 {
-  //***Driver settings********************************//
-  //commInterface can be I2C_MODE or SPI_MODE
-  //specify chipSelectPin using arduino pin names
-  //specify I2C address.  Can be 0x77(default) or 0x76
-
-  //For I2C, enable the following and disable the SPI section
-  mySensor.settings.commInterface = I2C_MODE;
-  mySensor.settings.I2CAddress = 0x76;
-
-  //For SPI enable the following and dissable the I2C section
-  //mySensor.settings.commInterface = SPI_MODE;
-  //mySensor.settings.chipSelectPin = 10;
-
-
-  //***Operation settings*****************************//
-
-  //renMode can be:
-  //  0, Sleep mode
-  //  1 or 2, Forced mode
-  //  3, Normal mode
-  mySensor.settings.runMode = 3; //Normal mode
-
-  //tStandby can be:
-  //  0, 0.5ms
-  //  1, 62.5ms
-  //  2, 125ms
-  //  3, 250ms
-  //  4, 500ms
-  //  5, 1000ms
-  //  6, 10ms
-  //  7, 20ms
-  mySensor.settings.tStandby = 0;
-
-  //filter can be off or number of FIR coefficients to use:
-  //  0, filter off
-  //  1, coefficients = 2
-  //  2, coefficients = 4
-  //  3, coefficients = 8
-  //  4, coefficients = 16
-  mySensor.settings.filter = 0;
-
-  //tempOverSample can be:
-  //  0, skipped
-  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-  mySensor.settings.tempOverSample = 1;
-
-  //pressOverSample can be:
-  //  0, skipped
-  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-    mySensor.settings.pressOverSample = 1;
-
-  //humidOverSample can be:
-  //  0, skipped
-  //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-  mySensor.settings.humidOverSample = 1;
+  bmeSensor.settings.commInterface = I2C_MODE;
+  bmeSensor.settings.I2CAddress = 0x76;
+  bmeSensor.settings.runMode = 3; //Normal mode
 
   Serial.begin(115200);
   Serial.print("Program Started\n");
@@ -79,13 +27,13 @@ void setup()
   u8g2.sendBuffer();
   delay(2000);
 
-  delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
-  mySensor.begin();
 }
 
 void loop() {
+  delay(5);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up claims sparkfun
+  bmeSensor.begin();
   Serial.print("register 0xD0: 0x");
-  int sensorType = mySensor.readRegister(0xD0);
+  int sensorType = bmeSensor.readRegister(0xD0);
   Serial.println(sensorType, HEX);
 
   u8g2.clearBuffer();
@@ -99,6 +47,9 @@ void loop() {
       sprintf(buf, "sensor: BMP");
       break;
     case 0x60:
+      sprintf(buf, "temp: %f", bmeSensor.readTempC());
+      u8g2.drawStr(0,50, buf);
+
       sprintf(buf, "sensor: BME");
       break;
     default:
